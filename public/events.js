4a4ddee2b6df1d00318feb62903a9283aa7be1f3
minkikim98 <minkikim98@gmail.com>
2021-02-11 12:50:46 -0800

    finished phases 2 and 3

--- public/events.js
+++ public/events.js
@@ -1,7 +1,26 @@
+console.log("test");
+
 window.addEventListener("DOMContentLoaded", async event => {
-    const res = await fetch("/kitten/image");
-    const json = await res.json();
-    console.log(json);
-    const pick = document.querySelector(".cat-pic");
-    pick.src = json.src;
+    // Phase 1: Fetches kitten picture upon first loadup
+    updatePic();
+
+    // Phase 2: New Pic Button
+    let newCatButton = document.getElementById("new-pic");
+    newCatButton.addEventListener("click", updatePic);
 });
+
+async function updatePic() {
+    let loader = document.querySelector(".loader");
+    loader.innerHTML = "Loading...";
+    const res = await fetch("/kitten/image");
+    if (res.ok) {
+        const json = await res.json();
+        const pick = document.querySelector(".cat-pic");
+        pick.src = json.src;
+    } else {
+        const json = await res.json();
+        let error = document.querySelector(".error");
+        error.innerHTML = json.message;
+    }
+    loader.innerHTML = "";
+}
